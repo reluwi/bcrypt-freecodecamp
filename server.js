20579c5d01ec0350636c632fe6d2f19b4ca02225
Renzo Osmillo <osmillo08@gmail.com>
2025-12-02 19:11:06 +0800

    bruh

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-const bcrypt      = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
